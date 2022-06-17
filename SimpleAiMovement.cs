99c3d3da10dcea5440a15683f3b096ba11ee5860
mcchopter <106834393+mcchopter@users.noreply.github.com>
2022-06-17 07:50:11 -0600

    Update SimpleAiMovement.cs

--- SimpleAiMovement.cs
+++ SimpleAiMovement.cs
@@ -7,17 +7,64 @@ public class SimpleAiMovement : MonoBehaviour
 {
     [SerializeField] Transform target;
     NavMeshAgent agent;
+
+    [SerializeField] Transform moveSpots;
+    public float speed;
+    public float startWaitTime;
+
+    private int randomSpot;
+    private float waitTime;
+
+    
+
+    // public Transform player;
+    private Rigidbody2D rb;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            target = other.transform;
+            Debug.Log(target);
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            target = null;
+            
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        rb = this.GetComponent<Rigidbody2D>();
+        target = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            Vector3 direction = target.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+
+        }
+
+
     }
+
+    
+
 }
